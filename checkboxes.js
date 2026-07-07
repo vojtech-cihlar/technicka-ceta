f30765a0644ab6da2de871dfe962376a28e1ad0f
Vojtěch Cihlář <voj.cihlar@gmail.com>
2026-07-08 00:40:56 +0200

    v1.2 - úpravy vzhled + aut. reset 3:00

--- checkboxes.js
+++ checkboxes.js
@@ -1,3 +1,33 @@
+/* ==========================================
+   AUTOMATICKÝ RESET CHECKBOXŮ VE 3:00
+========================================== */
+
+const now = new Date();
+
+// "Technický den" začíná ve 3:00
+const technicalDate = new Date(now);
+
+if (technicalDate.getHours() < 1) {
+    technicalDate.setDate(technicalDate.getDate() - 1);
+}
+
+const todayKey = technicalDate.toISOString().split("T")[0];
+const savedDay = localStorage.getItem("technicalDay");
+
+if (savedDay !== todayKey) {
+
+    // smaže všechny uložené checkboxy
+Object.keys(localStorage).forEach(key => {
+
+    if (/-\d+$/.test(key)) {
+        localStorage.removeItem(key);
+    }
+
+});
+
+    localStorage.setItem("technicalDay", todayKey);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const page = location.pathname
