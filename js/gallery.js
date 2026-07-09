434741b3ae8d970ea6989af5ec3f6bfc2b36f694
Vojtěch Cihlář <voj.cihlar@gmail.com>
2026-07-09 02:26:29 +0200

    v1.7 - galerie 2

--- js/gallery.js
+++ js/gallery.js
@@ -1,33 +1,74 @@
 import PhotoSwipeLightbox from "../libs/photoswipe/photoswipe-lightbox.esm.js";
 
-const button = document.getElementById("galleryButton");
+const galleryButton = document.getElementById("galleryButton");
 
-const folder = button.dataset.gallery;
-const count = Number(button.dataset.count);
+if (!galleryButton) {
+    throw new Error("Gallery button nebyl nalezen.");
+}
 
-const dataSource = [];
+const folder = galleryButton.dataset.gallery;
+const count = Number(galleryButton.dataset.count);
 
-for (let i = 1; i <= count; i++) {
+async function createDataSource() {
 
-    dataSource.push({
-        src: `${folder}${i}.jpg`,
-        width: 1600,
-        height: 900
+    const dataSource = [];
+
+    for (let i = 1; i <= count; i++) {
+
+        const src = `${folder}${i}.jpg`;
+
+        const image = new Image();
+
+        image.src = src;
+
+        await new Promise(resolve => {
+
+            image.onload = resolve;
+            image.onerror = resolve;
+
+        });
+
+        dataSource.push({
+
+            src,
+
+            width: image.naturalWidth || 1600,
+
+            height: image.naturalHeight || 900
+
+        });
+
+    }
+
+    return dataSource;
+
+}
+
+async function initGallery() {
+
+    const dataSource = await createDataSource();
+
+    const lightbox = new PhotoSwipeLightbox({
+
+        dataSource,
+
+        pswpModule: () =>
+            import("../libs/photoswipe/photoswipe.esm.js"),
+
+        showHideAnimationType: "fade",
+
+        bgOpacity: 0.88
+
+    });
+
+    lightbox.init();
+
+    galleryButton.addEventListener("click", () => {
+
+        lightbox.loadAndOpen(0);
+
     });
 
 }
 
-const lightbox = new PhotoSwipeLightbox({
-
-    dataSource,
-
-    pswpModule: () =>
-        import("../libs/photoswipe/photoswipe.esm.js")
-
-});
-
-button.addEventListener("click", () => {
-
-    lightbox.loadAndOpen(0);
-
-});
+initGallery();
